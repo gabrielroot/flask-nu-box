254d1727b43479c4c64a296df28a4c074aaeae1c
gabrielroot <gabrielfer.s88@gmail.com>
2023-03-28 00:13:14 -0300

    Refact: restapi - Separated urls.

--- nuBox/blueprints/restapi/__init__.py
+++ nuBox/blueprints/restapi/__init__.py
@@ -1,11 +1,4 @@
-from flask import Blueprint
-from flask_restful import Api
-from .resources.userResources import UsersResource, UserItemResource
-
-bp = Blueprint("restapi", __name__, url_prefix="/api/v1")
-api = Api(bp)
-api.add_resource(UsersResource, "/users/")
-api.add_resource(UserItemResource, "/user/<user_id>")
+from .urls import bp
 
 
 def init_app(app):
---
+++ nuBox/blueprints/restapi/urls.py
@@ -0,0 +1,8 @@
+from flask import Blueprint
+from flask_restful import Api
+from .resources.userResources import UsersResource, UserItemResource
+
+bp = Blueprint("restapi", __name__, url_prefix="/api/v1")
+api = Api(bp)
+api.add_resource(UsersResource, "/users/")
+api.add_resource(UserItemResource, "/user/<user_id>")
