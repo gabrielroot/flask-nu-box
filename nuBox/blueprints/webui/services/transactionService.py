ec4248ad035d5e2787d7168f8d01bc2454e73729
gabrielroot <gabrielfer.s88@gmail.com>
2023-03-29 13:14:46 -0300

    Style: Transaction - Shorthand to calculate values.

--- nuBox/blueprints/webui/services/transactionService.py
+++ nuBox/blueprints/webui/services/transactionService.py
@@ -6,14 +6,14 @@ def makeDepositOrWithdraw(transaction, box, balance):
         if balance.total - transaction.value < 0:
             return "Você não tem saldo suficiente para guardar este valor na caixinha."
 
-        box.value = box.value + transaction.value
-        balance.total = balance.total - transaction.value
+        box.value += transaction.value
+        balance.total -= transaction.value
     elif transaction.operation == TransactionOperation.WITHDRAW.name:
         if box.value - transaction.value < 0:
             return "Você tentou resgatar um valor maior do que o total guardado na caixinha."
 
-        box.value = box.value - transaction.value
-        balance.total = balance.total + transaction.value
+        box.value -= transaction.value
+        balance.total += transaction.value
 
 
 def makeDepositOrWithdrawAtBalance(transaction, balance):
