54f241569cbbb3ff47c3837d9f213884f3da7943
gabrielroot <gabrielfer.s88@gmail.com>
2023-04-17 21:17:50 -0300

    Test: Models - Covered all database models.

--- tests/models/test_models.py
+++ tests/models/test_models.py
@@ -1,8 +1,12 @@
-from nuBox.ext.database import Box as BoxModel, User as UserModel
+from datetime import datetime
+
+from nuBox.ext.database import Box as BoxModel
+from nuBox.ext.database import Transaction as TransactionModel
 from nuBox.ext.authentication.views.authView import create_user
+from nuBox.blueprints.webui.utils.OPTransactionEnum import TransactionOperation
 
 
-def test_create_first_user(app):
+def test_create_user(app):
     plain_password = 'pass123'
     user = create_user(username="test_user", password=plain_password)
 
@@ -11,9 +15,47 @@ def test_create_first_user(app):
     assert user.password != plain_password
 
 
-def test_create_box(app):
-    first_user = UserModel.query.one()
-    box = BoxModel(name='boxname', value=0, goal=100, user_id=first_user.id)
+def test_base_model(app, user):
+    box = BoxModel(name='base_box_test', value=0, goal=100, user_id=user.id)
+    box.persist()
+
+    now = datetime.utcnow()
+    assert type(box.createdAt) == type(now)
+
+    box.persist(True)
+    assert box.updatedAt > box.createdAt
+
+    box.remove(True)
+    assert box.deletedAt > box.createdAt
+    assert box.deleted is not None
+
+
+def test_create_box(app, user):
+    box = BoxModel(name='boxname', value=0, goal=100, user_id=user.id)
     box.persist()
 
     assert box.id is not None
+
+
+def test_create_transaction(app, user):
+    first_box = BoxModel.query.first()
+
+    transaction = TransactionModel(
+        operation=TransactionOperation.DEPOSIT.name,
+        value=100,
+        date=datetime.utcnow()
+    )
+
+    transactionBalance = transaction
+    transactionBalance.balance_id = user.balance.id
+    transactionBalance.persist()
+
+    transactionBox = transaction
+    transactionBox.box_id = first_box.id
+    transactionBox.persist()
+
+    transaction.persist()
+
+    assert transaction.id is not None
+    assert transactionBalance.id is not None
+    assert transactionBox.id is not None
