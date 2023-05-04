f12e3cba2b5e3a0d6299dec45c2b701bb8a49df8
gabrielroot <gabrielfer.s88@gmail.com>
2023-05-03 22:25:44 -0300

    Test: Filters - Covered app filters.

--- nuBox/ext/template_filters.py
+++ nuBox/ext/template_filters.py
@@ -5,9 +5,12 @@ import pytz
 
 def init_app(app):
     @app.template_filter()
-    def format_datetime(date_time, formatTo, tz=app.config.TIMEZONE):
+    def format_datetime(date_time,
+                        formatTo,
+                        tz=app.config.TIMEZONE):
         date_time = date_time.replace(tzinfo=pytz.utc)
-        return date_time.astimezone(pytz.timezone(tz)).strftime(formatTo)
+        return date_time.astimezone(pytz.timezone(tz)).\
+            strftime(formatTo)
 
     @app.template_filter()
     def format_money(value, toLocale=app.config.LOCALE):
